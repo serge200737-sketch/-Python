7bf4ae0e0113e08006aa5585054fc9b936c01a1a
exper <serge200737@gmail.com>
2026-06-28 11:23:47 +0300

    feat: добавлена работа с внешним API через requests

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Привет из PyCharm, GitHub!")
+import requests
+
+print("Запрашиваю случайную шутку...")
+response = requests.get("https://official-joke-api.appspot.com/random_joke")
+data = response.json()
+
+print(f"Сетап: {data['setup']}")
+print(f"Панчлайн: {data['punchline']}")
