3c1f40a595cefcc7ad71278e42c2fd39f7918174
exper <serge200737@gmail.com>
2026-06-28 21:32:44 +0300

    print("Этот код написан на втором компьютере!")

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-import requests
-
-print("Запрашиваю случайную шутку...")
-response = requests.get("https://official-joke-api.appspot.com/random_joke")
-data = response.json()
-
-print(f"Сетап: {data['setup']}")
-print(f"Панчлайн: {data['punchline']}")
+print("Этот код написан на втором компьютере!")
